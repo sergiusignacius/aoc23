127977b33c13551b1474af388bebbf079ea0b4fd
Sergio Silva <serge.a.silva@gmail.com>
2023-12-07 17:43:26 +0100

    make day 3 a little prettier

--- 3/3.py
+++ 3/3.py
@@ -9,107 +9,48 @@ def parse(input):
 def is_symbol(c):
     return not c.isdigit() and c != "."
 
-def check_surrounded_by_symbol(rn, start, end, numbers):
-    before = start - 1
-    after = end + 1
-    row = numbers[rn]
-    if before >= 0 and is_symbol(row[before]):
-        return True
-    elif after < len(row) and is_symbol(row[after]):
-        return True
-    for i in range(start - 1, end + 2):
-        prev_row = rn - 1
-        next_row = rn + 1
-        if i < 0 or i >= len(row):
-            continue
-        if prev_row >= 0 and is_symbol(numbers[prev_row][i]):
-            return True
-        elif next_row < len(row) and is_symbol(numbers[next_row][i]):
-            return True
-    
-    return False
-
 def solve(input):
-    result = 0
-    numbers_in_row = []
-    for i in range(len(input)):
-        numbers = [pos for (n, pos) in zip(input[i], range(len(input[i]))) if n.isdigit()]
-        numbers_in_row.append(numbers)
-    
-    for (r, ranges) in enumerate(numbers_in_row):
-        if len(ranges) == 0:
-            continue
-        start = ranges[0]
-        for i in range(1, len(ranges)):
-            if ranges[i] == ranges[i - 1] + 1: 
-                continue
-            else:
-                end = ranges[i - 1]
-                if check_surrounded_by_symbol(r, start, end, input):
-                    result += int(input[r][start:end+1])
-                start = ranges[i]
-        end = ranges[-1]
-        if check_surrounded_by_symbol(r, start, end, input):
-            result += int(input[r][start:end+1])
+    engine = parse(input)
 
-    return result
+    parts = []
+    start = None
+    for y in range(len(engine)):
+        parts_in_row = []
+        row = engine[y]
+        for x in range(len(row)):
+            c = row[x]
+            if c.isdigit() and start is None:
+                start = x
+            elif not c.isdigit() and start is not None:
+                end = x
+                parts_in_row.append((start, end - 1))
+                start = None
+        parts.append(parts_in_row)
 
-def is_star(c):
-    return c == "*"
+    engine_parts = []
+    gears = {}
+    for y in range(len(engine)):
+        for x in range(len(row)):
+            c = engine[y][x]
+            if is_symbol(c):
+                dc = [-1, 0, 1]
+                for dx in dc:
+                    for dy in dc:
+                        if dx == 0 and dy == 0:
+                            continue
+                        xx = x + dx
+                        yy = y + dy
+                        if 0 <= xx < len(row) and 0 <= yy < len(engine):
+                            if not is_symbol(engine[yy][xx]):
+                                in_range = [p for p in parts[yy] if p[0] <= xx <= p[1]]
+                                parts[yy] = [p for p in parts[yy] if p not in in_range]
+                                numbers = [int(engine[yy][s:e+1]) for (s, e) in in_range]
+                                engine_parts.extend(numbers)
+                                if c == "*":
+                                    if (x, y) not in gears:
+                                        gears[(x, y)] = []
+                                    gears[(x, y)].extend(numbers)
 
-def increase_star_adjacencies(stars, i, j, number):
-    if (i, j) not in stars:
-        stars[(i, j)] = []
-    stars[(i, j)].append(number)
+    return (sum(engine_parts), sum([v[0] * v[1] for v in gears.values() if len(v) == 2]))
 
-def check_surrounded_by_star(rn, start, end, numbers, stars):
-    before = start - 1
-    after = end + 1
-    row = numbers[rn]
-    number = int(row[start:end+1])
-    if before >= 0 and is_star(row[before]):
-        increase_star_adjacencies(stars, rn, before, number)
-    elif after < len(row) and is_star(row[after]):
-        increase_star_adjacencies(stars, rn, after, number)
-    for i in range(start - 1, end + 2):
-        prev_row = rn - 1
-        next_row = rn + 1
-        if i < 0 or i >= len(row):
-            continue
-        if prev_row >= 0 and is_star(numbers[prev_row][i]):
-            increase_star_adjacencies(stars, prev_row, i, number)
-        elif next_row < len(row) and is_star(numbers[next_row][i]):
-            increase_star_adjacencies(stars, next_row, i, number)
-
-def solve2(input):
-    result = 0
-    numbers_in_row = []
-    star_adjacencies = {}
-    for i in range(len(input)):
-        numbers = [pos for (n, pos) in zip(input[i], range(len(input[i]))) if n.isdigit()]
-        numbers_in_row.append(numbers)
-    
-    for (r, ranges) in enumerate(numbers_in_row):
-        if len(ranges) == 0:
-            continue
-        start = ranges[0]
-        for i in range(1, len(ranges)):
-            if ranges[i] == ranges[i - 1] + 1: 
-                continue
-            else:
-                end = ranges[i - 1]
-                check_surrounded_by_star(r, start, end, input, star_adjacencies)
-                start = ranges[i]
-        end = ranges[-1]
-        check_surrounded_by_star(r, start, end, input, star_adjacencies)
-
-    for (i, j) in star_adjacencies:
-        if len(star_adjacencies[(i, j)]) > 1:
-            ratio = 1
-            for number in star_adjacencies[(i, j)]:
-                ratio *= number
-            result += ratio
-
-    return result
-
-print(solve2(parse("3/input.txt")))
+solve("3/input.txt")
